6eb06c144439e88dd4e97cb7f1b1defb0a2211d3
Al.Sadeghi <Al.Sadeghi@SADEGHI-AL>
2023-12-13 16:21:13 +0330

    second commit

--- ConsoleApplication9.cpp
+++ ConsoleApplication9.cpp
@@ -6,107 +6,6 @@
 #include <windows.h>
 
 using namespace std;
-
-vector<int> round_down_scores(vector<double> student_scores) {
-	vector<int> result{};
-	
-
-	for (double i : student_scores) {
-		result.emplace_back(static_cast<int>(i));
-	}
-	return result;
-
-}
-
-struct ElectionResult {
-	// Name of the candidate
-	std::string name{};
-	// Number of votes the candidate has
-	int votes{};
-};
-
-int count_failed_students(vector<int> scores) {
-	int counter{};
-
-	for (int i : scores) {
-		if (i < 40) {
-			counter++;
-		}
-	}
-
-	return counter;
-}
-
-vector<int> above_threshold(vector<int> student_scores, int topScore) {
-	vector<int> result;
-
-	for (int i : student_scores) {
-		if (i >= topScore) {
-			result.push_back(i);
-		}
-	}
-
-	return result;
-}
-
-
-array<int , 4> letter_grades(int score) {
-	array<int, 4> result{};
-	const int lowest = 40;
-
-	int level = (score - lowest) / 4;
-	int add_num = 0;
-
-	for (int& i : result) {
-		i += lowest + add_num + 1;
-		add_num += level;
-	}
-
-	return result;
-
-}
-
-vector<string> student_ranking(vector<int> student_scores,vector<string> student_names) {
-	vector<string> result{};
-	string temp;
-	
-
-	for (int i = 0; i < student_scores.size();i++ ) {
-		result.emplace_back(to_string(i + 1) + ". " + student_names[i] + ": " + to_string(student_scores[i]));
-	}
-
-	return result;
-}
-
-vector<string> perfect_score(vector<int> student_scores, vector<string> student_names) {
-	int counter = 0;
-	vector<string> result;
-
-	for (int i : student_scores) {
-		if (i == 100) {
-			 result.push_back(student_names[counter]);
-		}
-		counter++;
-	}
-	return result;
-}
-
-int vote_count(ElectionResult &a) {
-	return a.votes;
-}
-
-vector<string> split_into_triplets(int number) {
-	vector<string> triplets;
-
-	for (int i = 0; i < to_string(number).length() / 3; i++) {
-		triplets.push_back(to_string(number % 1000));
-		number /= 1000;
-	}
-	triplets.push_back(to_string(number));
-
-	return triplets;
-}
-
 string one_num_detector(char num) {
 	switch (num)
 	{
@@ -218,12 +117,10 @@ string two_num_detector(char num_1 , char num_2) {
 
 string print(long long number) {
 	string num = to_string(number);
-	cout << num << endl;
 	int numLen = num.length();
 	reverse(num.begin(), num.end());
 	string result;
 	for (int i = numLen - 1; i >=0  ; i--) {
-		cout << num[i] << endl;
 		switch (i % 3)
 		{
 		case 2:
@@ -274,7 +171,6 @@ int main(int argc, char* argv[]) {
 	//vector<string> ranking{};
 	//vector<string> perfect_score_Name;
 	//int failed_count;
-	SetConsoleOutputCP(1256);
 	long long num{222'333'444};
 
 	cout<<print(num);
